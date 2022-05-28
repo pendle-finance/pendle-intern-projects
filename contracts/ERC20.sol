ca8c7f5dedef2d77160e46ca77d645a310fb31da
LongNguyen <longnguyenhsgs@gmail.com>
2022-05-28 17:38:58 +0800

    Finish

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,26 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.11;
 
 import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
+
+  uint private totalBalance;
+  
+  mapping(address => uint) private balances;
+  mapping(address => mapping(address => uint)) private allowBalances;
+  
+  constructor(){
+    totalBalance = 100000;
+    balances[msg.sender] = 100000;
+  }
   function totalSupply() external view returns (uint256) {
-    return 0;
+    return totalBalance;
   }
 
   function balanceOf(address account) external view returns (uint256) {
-    return 0;
+    return balances[account];
   }
 
   function transfer(address to, uint256 amount) external returns (bool) {
+    // check enough balance
+    require(balances[msg.sender]>=amount,"Insufficient amount");
+    // decrease the value 
+    balances[msg.sender]-=amount;
+    // increase the value
+    balances[to]+=amount;
+    emit Transfer(msg.sender, to, amount);
     return true;
   }
 
   function allowance(address owner, address spender) external view returns (uint256) {
-    return 0;
+    return allowBalances[owner][spender];
   }
 
   function approve(address spender, uint256 amount) external returns (bool) {
+    allowBalances[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
     return true;
   }
 
@@ -29,6 +48,15 @@ contract ERC20 is IERC20 {
     address to,
     uint256 amount
   ) external returns (bool) {
+    // check balance
+    require(balances[from]>=amount,"Insufficient amount");
+    // check allowance
+    require(allowBalances[from][to]>=amount,"Insufficient allowance");
+    // decrease value
+    balances[from]-=amount;
+    //increase value
+    balances[to]+=amount;
+    emit Transfer(from, to, amount);
     return true;
   }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,20 +1,19 @@
 import { expect } from "chai";
-import { utils } from "ethers";
+import { utils, Wallet } from "ethers";
 import { ethers, waffle } from "hardhat";
 import { deploy, evm_revert, evm_snapshot } from "./helpers/hardhat-helpers";
-import { TestContract } from "../typechain";
+import { ERC20, TestContract } from "../typechain";
 
 describe("TestContract", () => {
-  const [admin] = waffle.provider.getWallets();
+  const [admin,addr1,addr2] = waffle.provider.getWallets();
+  //Wallet;
   let globalSnapshotId;
   let snapshotId;
-  let testContract: TestContract;
-
+  let newERC20: ERC20;
   before(async () => {
     globalSnapshotId = await evm_snapshot();
 
-    testContract = await deploy<TestContract>("TestContract", []);
-    await testContract.setTotal(100);
+    newERC20 = await deploy<ERC20>("ERC20", []);
 
     snapshotId = await evm_snapshot();
   });
@@ -27,18 +26,59 @@ describe("TestContract", () => {
   beforeEach(async () => {
     await revertSnapshot();
   });
+  //   0x6824c889f6EbBA8Dac4Dd4289746FCFaC772Ea56
+  //   0xCFf94465bd20C91C86b0c41e385052e61ed49f37
+  //   0xEBAf3e0b7dBB0Eb41d66875Dd64d9F0F314651B3
+  //   0xbFe6D5155040803CeB12a73F8f3763C26dd64a92
+  // it("increases total successfully", async () => {
+  //   await testContract.increaseTotal(100);
 
-  it("increases total successfully", async () => {
-    await testContract.increaseTotal(100);
+  //   let curTotal = await testContract.getTotal();
+  //   expect(curTotal).to.be.eq(200);
+  // });
+  it("test get total supply",async () => {
+    let total = await newERC20.totalSupply();
+    expect(total).to.be.eq(100000);
+  })
+  
+  it("test get balances of admin",async () => {
+    let total = await newERC20.balanceOf(admin.address);
+    expect(total).to.be.eq(100000);
+  })
 
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(200);
-  });
+  it("test get balances of user",async () => {
+    let total = await newERC20.connect(addr1).balanceOf(addr1.address);
+    expect(total).to.be.eq(0);
+  })
 
-  it("decreases total successfully", async () => {
-    await testContract.decreaseTotal(50);
+  it("test transfer",async () => {
+    await newERC20.connect(admin).transfer(addr1.address,1000);
+    let total2 = await newERC20.connect(admin).balanceOf(addr1.address);
+    expect(total2).to.be.eq(1000);
+  })
+  
+  it("test allowance",async () => {
+    let allow_ori  = await newERC20.allowance(admin.address,addr1.address);
+    expect(allow_ori).to.be.eq(0);
+    await newERC20.approve(addr1.address,2000);
+    let allow_after  = await newERC20.allowance(admin.address,addr1.address);
+    expect(allow_after).to.be.eq(2000);
+  })
 
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(50);
-  });
+  it("test transferFrom",async () => {
+    let allow_ori  = await newERC20.allowance(admin.address,addr1.address);
+    expect(allow_ori).to.be.eq(0);
+    await newERC20.approve(addr1.address,2000);
+    let allow_after  = await newERC20.allowance(admin.address,addr1.address);
+    expect(allow_after).to.be.eq(2000);
+    await newERC20.connect(addr1).transferFrom(admin.address,addr1.address,1000);
+    let ad1Balance = await newERC20.balanceOf(addr1.address);
+    expect(ad1Balance).to.be.eq(1000);
+  })
+  // it("decreases total successfully", async () => {
+  //   await testContract.decreaseTotal(50);
+
+  //   let curTotal = await testContract.getTotal();
+  //   expect(curTotal).to.be.eq(50);
+  // });
 });
