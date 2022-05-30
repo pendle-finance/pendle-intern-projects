4055fe13f2ce160bc7ba66fba36da7da78929563
matthew-chua <mattchua2603@gmail.com>
2022-05-30 11:13:58 +0800

    updates

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,36 +4,40 @@ pragma solidity ^0.8.0;
 import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
-  uint256 public totalSupply;
+  uint256 public _totalSupply;
   mapping(address => uint256) public ownership;
   mapping(address => mapping(address => uint256)) public allowances;
 
   constructor(uint256 initialSupply) {
-    totalSupply = initialSupply;
+    _totalSupply = initialSupply;
     ownership[msg.sender] = initialSupply;
   }
 
-  function getTotalSupply() external view returns (uint256) {
-    return totalSupply;
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
   }
 
   function balanceOf(address account) external view returns (uint256) {
     return ownership[account];
   }
 
-  function transfer(address to, uint256 amount) external returns (bool) {
-    require(ownership[msg.sender] >= amount, "Insufficient funds");
-    ownership[msg.sender] -= amount;
-    ownership[to] += amount;
-    return true;
-  }
-
   function allowance(address owner, address spender) external view returns (uint256) {
     return allowances[owner][spender];
   }
 
+  function transfer(address to, uint256 amount) external returns (bool) {
+    require(ownership[msg.sender] >= amount, "Insufficient funds");
+    require(to != address(0), "Sending to 0 address");
+    ownership[msg.sender] -= amount;
+    ownership[to] += amount;
+    emit Transfer(msg.sender, to, amount);
+    return true;
+  }
+
   function approve(address spender, uint256 amount) external returns (bool) {
+    require(spender != address(0), "Spender cannot be 0 address");
     allowances[msg.sender][spender] = amount;
+    emit Approval(msg.sender, spender, amount);
     return true;
   }
 
@@ -44,18 +48,21 @@ contract ERC20 is IERC20 {
   ) external returns (bool) {
     require(allowances[from][to] >= amount, "Receiver not approved");
     require(ownership[from] >= amount, "Insufficient balance");
+    require(to != address(0), "Cannot transfer to 0 address");
+    require(from != address(0), "Cannot transfer from 0 address");
     ownership[from] -= amount;
     ownership[to] += amount;
+    emit Transfer(from, to, amount);
     return true;
   }
 
   function mint(address to, uint256 amount) public {
     ownership[to] += amount;
-    totalSupply += amount;
+    _totalSupply += amount;
   }
 
   function burn(address from, uint256 amount) public {
     ownership[from] -= amount;
-    totalSupply -= amount;
+    _totalSupply -= amount;
   }
 }
--- test/erc20test.ts
+++ test/erc20test.ts
@@ -1,83 +1,96 @@
-import { expect } from "chai";
-import { utils } from "ethers";
-import { ethers, waffle } from "hardhat";
-import { deploy, evm_revert, evm_snapshot } from "./helpers/hardhat-helpers";
-import { ERC20 } from "../typechain";
+import {expect} from 'chai';
+import {utils} from 'ethers';
+import {ethers, waffle} from 'hardhat';
+import {deploy, evm_revert, evm_snapshot} from './helpers/hardhat-helpers';
+import {ERC20} from '../typechain';
 
-describe("ERC20 Test", () => {
-    let [admin] = waffle.provider.getWallets();
+describe('ERC20 Test', () => {
+  let [admin] = waffle.provider.getWallets();
 
-    let globalSnapshotId;
-    let snapshotId;
-    let erc20: ERC20;
-    
-    before(async () => {
-        globalSnapshotId = await evm_snapshot();
-        erc20 = await deploy<ERC20>("ERC20", [1000]);
-        snapshotId = await evm_snapshot();
-    });
+  let globalSnapshotId;
+  let snapshotId;
+  let erc20: ERC20;
 
-    const revertSnapshot = async () => {
-        await evm_revert(snapshotId);
-        snapshotId = await evm_snapshot();
-    }
+  before(async () => {
+    globalSnapshotId = await evm_snapshot();
+    erc20 = await deploy<ERC20>('ERC20', [1000]);
+    snapshotId = await evm_snapshot();
+  });
 
-    beforeEach(async () => {
-        await revertSnapshot();
-    })
+  const revertSnapshot = async () => {
+    await evm_revert(snapshotId);
+    snapshotId = await evm_snapshot();
+  };
 
-    it("totalSupply is correct", async () => {
-        const total = await erc20.getTotalSupply();
-        expect(total).to.be.eq(1000);
-    });
+  beforeEach(async () => {
+    await revertSnapshot();
+  });
 
-    it("approval works", async () => {
-        await erc20.approve("0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9", 200);
-        const allowance = await erc20.allowance(admin.address, "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9");
-        expect(allowance).to.be.eq(200);  
-    });
+  it('totalSupply is correct', async () => {
+    const total = await erc20.totalSupply();
+    expect(total).to.be.eq(1000);
+  });
 
-    it("balanceOf works", async () => {
-        const testWallet = "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9";
-        await erc20.transfer(testWallet, 200);
-        const testWalletBalance = await erc20.balanceOf(testWallet);
-        expect(testWalletBalance).to.be.eq(200);
-    });
+  it('approval works', async () => {
+    await erc20.approve('0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9', 200);
+    const allowance = await erc20.allowance(admin.address, '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9');
+    expect(allowance).to.be.eq(200);
+  });
 
-    it("transferFrom works", async () => {
-        const testWallet = "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9";
-        await erc20.approve(testWallet, 200);
-        await erc20.transferFrom(admin.address, testWallet, 200);
-        const testWalletBalance = await erc20.balanceOf(testWallet);
-        expect(testWalletBalance).to.be.eq(200);
-    });
+  it('balanceOf works', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.transfer(testWallet, 200);
+    const testWalletBalance = await erc20.balanceOf(testWallet);
+    expect(testWalletBalance).to.be.eq(200);
+  });
 
-    //this test is meant to fail, not sure what is the right way to write a test like this
-    it("transferring more than approved", async () => {
-        const testWallet = "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9";
-        await erc20.approve(testWallet, 200);
-        await erc20.transferFrom(admin.address, testWallet, 400);
-        const testWalletBalance = await erc20.balanceOf(testWallet);
-        expect(testWalletBalance).to.be.eq(0);
-    });
+  it('transferFrom works', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.approve(testWallet, 200);
+    await erc20.transferFrom(admin.address, testWallet, 200);
+    const testWalletBalance = await erc20.balanceOf(testWallet);
+    expect(testWalletBalance).to.be.eq(200);
+  });
 
-    it("mint works", async () => {
-        const testWallet = "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9";
-        await erc20.mint(testWallet, 200);
-        const testWalletBalance = await erc20.balanceOf(testWallet);
-        const newTotalBalance = await erc20.getTotalSupply();
-        expect(testWalletBalance).to.be.eq(200);
-        expect(newTotalBalance).to.be.eq(1200);
-    })
+  //this test is meant to fail, not sure what is the right way to write a test like this
+  it('transferring more than approved', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.approve(testWallet, 200);
+    await expect(erc20.transferFrom(admin.address, testWallet, 400)).to.be.revertedWith('Receiver not approved');
+  });
 
-    it("burn works", async () => {
-        const testWallet = "0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9";
-        await erc20.transfer(testWallet, 200);
-        await erc20.burn(testWallet, 100);
-        const testWalletBalance = await erc20.balanceOf(testWallet);
-        const newTotalBalance = await erc20.getTotalSupply();
-        expect(testWalletBalance).to.be.eq(100);
-        expect(newTotalBalance).to.be.eq(900);
-    })
-    
-})
+  it('mint works', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.mint(testWallet, 200);
+    const testWalletBalance = await erc20.balanceOf(testWallet);
+    const newTotalBalance = await erc20.totalSupply();
+    expect(testWalletBalance).to.be.eq(200);
+    expect(newTotalBalance).to.be.eq(1200);
+  });
+
+  it('burn works', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.transfer(testWallet, 200);
+    await erc20.burn(testWallet, 100);
+    const testWalletBalance = await erc20.balanceOf(testWallet);
+    const newTotalBalance = await erc20.totalSupply();
+    expect(testWalletBalance).to.be.eq(100);
+    expect(newTotalBalance).to.be.eq(900);
+  });
+
+  it('transfer() emits transfer', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await expect(erc20.transfer(testWallet, 200)).to.emit(erc20, 'Transfer').withArgs(admin.address, testWallet, 200);
+  });
+
+  it('approve() emits approval', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await expect(erc20.approve(testWallet, 200)).to.emit(erc20, 'Approval').withArgs(admin.address, testWallet, 200);
+  });
+
+  it('transferFrom() emits transfer', async () => {
+    const testWallet = '0x0264C3e4FA4E1eb38123A39776Ea3485179eFaC9';
+    await erc20.approve(testWallet, 200);
+    await expect( erc20.transferFrom(admin.address, testWallet, 200)).to.emit(erc20, 'Transfer').withArgs(admin.address, testWallet, 200);
+  })
+});
