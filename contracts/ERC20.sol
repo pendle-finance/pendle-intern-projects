1bb62185010e2b4db71168e7c8fcae84eedff4b6
victortanzy123 <83770436+victortanzy123@users.noreply.github.com>
2022-05-30 16:10:00 +0800

    first commit

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,31 +4,199 @@ pragma solidity ^0.8.0;
 import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
-  function totalSupply() external view returns (uint256) {
-    return 0;
-  }
 
-  function balanceOf(address account) external view returns (uint256) {
-    return 0;
+
+   /*///////////////////////////////////////////////////////////////
+                        State Variables:
+    //////////////////////////////////////////////////////////////*/
+    uint256 private totalSupply_;
+    uint256 public maxSupplyMintPerWallet = 1 ether;
+    uint256 public maxSupply = 100 ether; 
+    
+    string private name_;
+    string private symbol_;
+
+    address private admin_;
+
+     /*///////////////////////////////////////////////////////////////
+                        Mappings:
+    //////////////////////////////////////////////////////////////*/
+    mapping(address => uint256) private balances;
+
+    mapping(address => mapping(address => uint)) private allowances;
+
+    mapping(address => uint256) private userMintTracker;
+
+
+         /*///////////////////////////////////////////////////////////////
+                        Events:
+    //////////////////////////////////////////////////////////////*/
+    event AdminTransfer(address oldAdmin, address newAdmin);
+
+     /*///////////////////////////////////////////////////////////////
+                          Modifiers
+    //////////////////////////////////////////////////////////////*/
+
+    modifier onlyAdmin() {
+      require(msg.sender == admin_, "VTZY: Unauthorised user.");
+      _;
+    }
+
+    modifier safeMintAmount(uint256 amount) {
+      require(userMintTracker[msg.sender] + amount <= maxSupplyMintPerWallet, "VTZY: Total minting amount exceeds limit.");
+      require(totalSupply_ + amount <= maxSupply, "VTZY: Total supply exceeds max supply.");
+      _;
+    }
+
+         /*///////////////////////////////////////////////////////////////
+                          Constructor
+    //////////////////////////////////////////////////////////////*/
+    constructor(string memory _name, string memory _symbol) {
+      name_ = _name;
+      symbol_ = _symbol;
+      admin_ = msg.sender;
+      _mint(msg.sender, 1 ether);
+    }
+
+             /*///////////////////////////////////////////////////////////////
+                          Main Functions
+    //////////////////////////////////////////////////////////////*/
+
+  function approve(address spender, uint256 amount) external returns (bool) {
+      allowances[msg.sender][spender] = amount;
+
+      emit Approval(msg.sender, spender, amount);
+      return true;
   }
 
   function transfer(address to, uint256 amount) external returns (bool) {
+    address addressToTransfer = msg.sender;
+    _transfer(addressToTransfer, to, amount);
     return true;
   }
 
-  function allowance(address owner, address spender) external view returns (uint256) {
-    return 0;
-  }
-
-  function approve(address spender, uint256 amount) external returns (bool) {
-    return true;
-  }
-
+  
   function transferFrom(
     address from,
     address to,
     uint256 amount
   ) external returns (bool) {
+    address spender = msg.sender;
+    _allowanceExpenditure(from, spender, amount);
+    _transfer(from, to, amount);
+
     return true;
   }
+
+  function mint(uint256 amount) external safeMintAmount(amount){
+    address owner = msg.sender;
+    userMintTracker[owner] += amount;
+    _mint(owner, amount);
+  }
+
+  function adminMint(uint256 amount) external onlyAdmin {
+    address owner = msg.sender;
+    require(totalSupply_ + amount <= maxSupply, "VTZY: Total supply exceeds max supply.");
+    _mint(owner, amount);
+  }
+
+  function burn(address user, uint256 amount) external {
+    if(user == msg.sender) {
+      _burn(msg.sender, amount);
+    } else {
+      _allowanceExpenditure(user, msg.sender, amount);
+      _burn(user, amount);
+    }
+  }
+
+  
+         /*///////////////////////////////////////////////////////////////
+                          Internal/Private Functions
+    //////////////////////////////////////////////////////////////*/
+
+  function _mint(address user, uint256 amount) internal virtual {
+    require(user != address(0), "VTZY: Null address not allowed.");
+    
+    // Update states first:
+    totalSupply_ += amount;
+    balances[user] += amount;
+  }
+
+  function _allowanceExpenditure(address owner, address spender, uint256 amount) internal virtual {
+    uint256 givenAllowance = allowances[owner][spender];
+
+    require(givenAllowance >= amount, "VTZY: Invalid allowance amount specified.");
+
+    allowances[owner][spender] -= amount;
+
+  }
+
+  function _transfer(address from, address to, uint256 amount) internal virtual {
+
+    uint256 userBalance = balances[from];
+    require(userBalance >= amount, "VTZY: Insufficient existing funds.");
+
+    balances[from] -= amount;
+    balances[to] += amount;
+
+    emit Transfer(from, to, amount);
+  }
+
+  function _burn(address user, uint256 amount) internal virtual {
+    require(user != address(0), "VTZY: Cannot burn from null address");
+
+    uint256 currentBalance = balances[user];
+
+    require(amount <= currentBalance, "VTZY: Insufficient existing funds to burn");
+    balances[user] -= amount;
+    totalSupply_ -= amount;
+
+    emit Transfer(user, address(0), amount);
+  }
+
+         /*///////////////////////////////////////////////////////////////
+                         Admin Functions
+    //////////////////////////////////////////////////////////////*/
+    function transferAdminRights(address newAdmin) external onlyAdmin {
+      require(newAdmin != msg.sender && newAdmin != address(0), "VTZY: Invalid admin address specified");
+      admin_ = newAdmin;
+
+      emit AdminTransfer(msg.sender, newAdmin);
+    }
+
+    function setMaxSupply(uint newMaxSupply) external onlyAdmin {
+      require(newMaxSupply > totalSupply_, "VTZY: Invalid new maximum supply set");
+      maxSupply = newMaxSupply;
+    }
+
+  
+       /*///////////////////////////////////////////////////////////////
+                Standard ERC20 Getter Functions
+    //////////////////////////////////////////////////////////////*/
+
+    function name() external view returns (string memory) {
+      return name_;
+    }
+    
+    function symbol() external view returns (string memory) {
+      return symbol_;
+    }
+
+   function totalSupply() external view returns (uint256) {
+    return totalSupply_;
+  }
+
+  function balanceOf(address account) external view returns (uint256) {
+    return balances[account];
+  }
+
+  function allowance(address owner, address spender) external view returns (uint256) {
+    return allowances[owner][spender];
+  }
+
+  function admin() external view returns(address) {
+    return admin_;
+  }
+ 
+
 }
--- test/test.ts
+++ test/test.ts
@@ -2,19 +2,60 @@ import { expect } from "chai";
 import { utils } from "ethers";
 import { ethers, waffle } from "hardhat";
 import { deploy, evm_revert, evm_snapshot } from "./helpers/hardhat-helpers";
-import { TestContract } from "../typechain";
+import { TestContract, ERC20 } from "../typechain";
+import { ZERO_ADDRESS, _1E18 } from "./helpers/Constants";
 
-describe("TestContract", () => {
-  const [admin] = waffle.provider.getWallets();
+// describe("TestContract", () => {
+//   const [admin] = waffle.provider.getWallets();
+//   let globalSnapshotId;
+//   let snapshotId;
+//   let testContract: TestContract;
+
+//   before(async () => {
+//     globalSnapshotId = await evm_snapshot();
+
+//     testContract = await deploy<TestContract>("TestContract", []);
+//     await testContract.setTotal(100);
+
+//     snapshotId = await evm_snapshot();
+//   });
+
+//   async function revertSnapshot() {
+//     await evm_revert(snapshotId);
+//     snapshotId = await evm_snapshot();
+//   }
+
+//   beforeEach(async () => {
+//     await revertSnapshot();
+//   });
+
+//   it("increases total successfully", async () => {
+//     await testContract.increaseTotal(100);
+
+//     let curTotal = await testContract.getTotal();
+//     expect(curTotal).to.be.eq(200);
+//   });
+
+//   it("decreases total successfully", async () => {
+//     await testContract.decreaseTotal(50);
+
+//     let curTotal = await testContract.getTotal();
+//     expect(curTotal).to.be.eq(50);
+//   });
+// });
+
+
+describe("Testing VTZY ERC20 Contract", () => {
+  const [admin, minter1, minter2] = waffle.provider.getWallets();
   let globalSnapshotId;
   let snapshotId;
-  let testContract: TestContract;
+  let VTZYContract: ERC20;
 
   before(async () => {
     globalSnapshotId = await evm_snapshot();
 
-    testContract = await deploy<TestContract>("TestContract", []);
-    await testContract.setTotal(100);
+    // Deploy contract:
+    VTZYContract = await deploy<ERC20>("ERC20", ["Vtzy Token", "VTZY"]);
 
     snapshotId = await evm_snapshot();
   });
@@ -28,17 +69,283 @@ describe("TestContract", () => {
     await revertSnapshot();
   });
 
-  it("increases total successfully", async () => {
-    await testContract.increaseTotal(100);
+ describe("Deployment Status", () => {
+  it("should have a name vtzy and symbol VTZY", async () => {
 
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(200);
+    let name = await VTZYContract.name();
+    expect(name).to.be.eq("Vtzy Token");
+
+    let symbol = await VTZYContract.symbol();
+    expect(symbol).to.be.eq("VTZY");
   });
 
-  it("decreases total successfully", async () => {
-    await testContract.decreaseTotal(50);
+  it("should have an initial supply of 1 ether by owner", async () => {
 
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(50);
+    let curSupply = await VTZYContract.totalSupply();
+    let ownerBalance = await VTZYContract.balanceOf(admin.address);
+
+    expect(ownerBalance).to.be.eq(_1E18)
+    expect(curSupply).to.be.eq(ownerBalance);
   });
+ })
+
+
+ describe("Admin Rights", () => {
+  it("should ALLOW admin to transfer ownership rights and EMIT AdminTransfer Event", async () => {
+
+    let adminAddress = await VTZYContract.admin();
+    expect(adminAddress).to.be.eq(admin.address)
+
+    // Transfer ownership to minter1 and check for AdminTransfer Event:
+    await expect(VTZYContract.transferAdminRights(minter1.address)).to.emit(VTZYContract, "AdminTransfer").withArgs(admin.address, minter1.address);
+
+    // Check if the admin of the contract has been transferred to minter1:
+    adminAddress = await VTZYContract.admin();
+    expect(adminAddress).to.be.eq(minter1.address)
+  });
+
+  it("should ALLOW admin to set new maximum supply of tokens as long as it is greater than the existing total supply", async () => {
+
+    let curSupply = await VTZYContract.totalSupply();
+    expect(curSupply).to.be.eq(_1E18)
+
+    // Expect default maxSupply to be 100 ether worth of tokens
+    let curMaxSupply = await VTZYContract.maxSupply();
+    expect(curMaxSupply).to.be.eq(_1E18.mul(100))
+
+    // Update the new Max Supply to be 50 ether worth of tokens instead:
+    let newMaxSupply = ethers.utils.parseUnits('50.0');
+
+    await VTZYContract.setMaxSupply(newMaxSupply)
+
+    // Expect the Max Supply to be 50 ether worth of tokens now
+    curMaxSupply = await VTZYContract.maxSupply();
+    expect(curMaxSupply).to.be.eq(_1E18.mul(50))
+
+
+    // Since current total supply is 1 ether worth of tokens, attempt to set maxSupply < 1 ether i.e. 0.5 ether worth of token:
+    let invalidNewMaxSupply = ethers.utils.parseUnits('0.5');
+    await expect(VTZYContract.setMaxSupply(invalidNewMaxSupply)).to.be.revertedWith("VTZY: Invalid new maximum supply set")
+  });
+ })
+
+
+  describe("Minting Of Tokens", () => {
+    it("should ONLY ALLOW admin to mint more than 1 ether worth of tokens via adminMint Function", async () => {
+
+
+      let mintAmount = ethers.utils.parseUnits("2.0")
+  
+      await VTZYContract.adminMint(mintAmount);
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      let curSupply = await VTZYContract.totalSupply();
+  
+  
+      expect(ownerBalance).to.be.eq(_1E18.mul(3));
+      expect(ownerBalance).to.be.eq(curSupply);
+      
+      // Expect revert from non-admin when calling adminMint function:
+      await expect( VTZYContract.connect(minter1).adminMint(mintAmount)).to.be.revertedWith("VTZY: Unauthorised user.")
+    });
+  
+    it("should ALLOW public to mint 1 ether worth of tokens", async () => {
+  
+    
+      let minter1Balance = await VTZYContract.balanceOf(minter1.address);
+      expect(minter1Balance).to.be.eq(0)
+  
+      let mintAmount = ethers.utils.parseUnits("1.0")
+  
+      await VTZYContract.connect(minter1).mint(mintAmount);
+      minter1Balance = await VTZYContract.balanceOf(minter1.address);
+      expect(minter1Balance).to.be.eq(_1E18)
+  
+      let curSupply = await VTZYContract.totalSupply();
+      expect(curSupply).to.be.eq(_1E18.mul(2));
+    });
+  
+    it("should REVERT if a non-admin tries to mint more than 1 ether worth of tokens", async () => {
+  
+    
+      let minter1Balance = await VTZYContract.balanceOf(minter1.address);
+      expect(minter1Balance).to.be.eq(0)
+  
+      let mintAmount = ethers.utils.parseUnits("1.0")
+  
+      await VTZYContract.connect(minter1).mint(mintAmount);
+      minter1Balance = await VTZYContract.balanceOf(minter1.address);
+      expect(minter1Balance).to.be.eq(_1E18)
+  
+      // Expect revert when user tries to mint another:
+      await expect( VTZYContract.connect(minter1).mint(mintAmount)).to.be.revertedWith("VTZY: Total minting amount exceeds limit.")
+  
+      // Double confirm that supply remains as 2 ether worth of tokens
+      let curSupply = await VTZYContract.totalSupply();
+      expect(curSupply).to.be.eq(_1E18.mul(2));
+    });
+
+    it("should REVERT if an attempt to mint more than 100 ether of max supply is made", async () => {
+
+      // Current supply is 1 ether (admin minted when deploying contract)
+      let mintAmount = ethers.utils.parseUnits("100.0")
+  
+      await expect(VTZYContract.adminMint(mintAmount)).to.be.revertedWith("VTZY: Total supply exceeds max supply.");
+    });
+
+    
+  })
+
+  describe('Transfers & Approvals', () => { 
+    it("should able to transfer tokens", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let minter1Balance = await VTZYContract.balanceOf(minter1.address)
+      expect(minter1Balance).to.be.eq(0);
+  
+      let transferAmount = ethers.utils.parseUnits("0.5")
+  
+      await VTZYContract.transfer(minter1.address, transferAmount);
+  
+      // Query new balances:
+      ownerBalance = await VTZYContract.balanceOf(admin.address);
+      minter1Balance = await VTZYContract.balanceOf(minter1.address)
+      expect(ownerBalance).to.be.eq(_1E18.div(2))
+      expect(ownerBalance).to.be.eq(minter1Balance)
+  
+  
+    
+    });
+
+    it("should EMIT a Transfer Event when transferring tokens", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let minter1Balance = await VTZYContract.balanceOf(minter1.address)
+      expect(minter1Balance).to.be.eq(0);
+  
+      let transferAmount = ethers.utils.parseUnits("0.5")
+  
+      await expect(VTZYContract.transfer(minter1.address, transferAmount)).to.emit(VTZYContract, 'Transfer').withArgs(admin.address, minter1.address, transferAmount)
+
+    });
+  
+    it("should NOT transfer tokens if insufficient funds", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let minter1Balance = await VTZYContract.balanceOf(minter1.address)
+      expect(minter1Balance).to.be.eq(0);
+  
+      // Attempt to transfer more than existing fund (1.0 ether worth of token)
+      let transferAmount = ethers.utils.parseUnits("2.0")
+  
+      await expect(VTZYContract.transfer(minter1.address, transferAmount)).to.be.revertedWith("VTZY: Insufficient existing funds.");
+    
+    });
+
+
+    it("should EMIT an Approval Event should the approve function be called", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let validApproveAmount = ethers.utils.parseUnits('0.5')
+  
+     await expect(VTZYContract.approve(minter1.address, validApproveAmount)).to.emit(VTZYContract, "Approval").withArgs(admin.address, minter1.address, validApproveAmount);
+    
+    });
+  
+  
+    it("should ALLOW transfer of funds ONLY when sufficient allowance is given to a spender", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let validApproveAmount = ethers.utils.parseUnits('0.5')
+  
+     await VTZYContract.approve(minter1.address, validApproveAmount);
+  
+     // Tranfer Admin's funds by minter1:
+     await expect(VTZYContract.connect(minter1).transferFrom(admin.address, minter2.address, validApproveAmount)).to.emit(VTZYContract, 'Transfer').withArgs(admin.address, minter2.address, validApproveAmount)
+  
+      // Expect minter2's balance to be 0.5 ether worth of tokens
+     let minter2Balance = await VTZYContract.balanceOf(minter2.address);
+     ownerBalance = await VTZYContract.balanceOf(admin.address);
+     expect(minter2Balance).to.be.eq(_1E18.div(2));
+     expect(ownerBalance).to.be.eq(_1E18.div(2))
+  
+      // Attempt to transfer more than existing fund (additional 0.1 ether worth of token)
+      let invalidTransferAmount = ethers.utils.parseUnits("0.1")
+  
+      await expect(VTZYContract.connect(minter1).transferFrom(admin.address,minter2.address, invalidTransferAmount)).to.be.revertedWith("VTZY: Invalid allowance amount specified.");
+    
+    });
+   })
+
+   describe("Burn Transactions", () => {
+    it("should EMIT a Transfer Event when the burn function is successfully called upon", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let validBurnAmount = ethers.utils.parseUnits('0.5')
+      
+      // Existing balance - 1.0 ether worth of tokens, self-call burn function by admin
+     await expect(VTZYContract.burn(admin.address, validBurnAmount)).to.emit(VTZYContract, "Transfer").withArgs(admin.address, ZERO_ADDRESS, validBurnAmount);
+
+     // Balance should be updated accordingly after a successful burn transaction
+     ownerBalance = await VTZYContract.balanceOf(admin.address);
+     expect(ownerBalance).to.be.eq(_1E18.div(2));
+    
+    });
+
+    it("should ONLY burn when an amount specified is LESS than the balance of the owner", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      // Existing balance of admin is 1.0 ether worth of tokens:
+      let invalidBurnAmount = ethers.utils.parseUnits('2.0')
+      
+      // Should revert if an amount greater than current balance is specified to be burn
+     await expect(VTZYContract.burn(admin.address, invalidBurnAmount)).to.be.revertedWith("VTZY: Insufficient existing funds to burn");
+    });
+
+    it("should ALLOW a spender to burn an amount within the allowance specified by the owner", async () => {
+  
+    
+      let ownerBalance = await VTZYContract.balanceOf(admin.address);
+      expect(ownerBalance).to.be.eq(_1E18)
+  
+      let validApproveAmount = ethers.utils.parseUnits('0.5')
+  
+      // Call approve function and expect an Approval Event to be emitted
+     await expect(VTZYContract.approve(minter1.address, validApproveAmount)).to.emit(VTZYContract, "Approval").withArgs(admin.address, minter1.address, validApproveAmount);
+  
+     // Tranfer Admin's funds by minter1:
+     await expect(VTZYContract.connect(minter1).burn(admin.address, validApproveAmount)).to.emit(VTZYContract, 'Transfer').withArgs(admin.address, ZERO_ADDRESS, validApproveAmount)
+  
+      // Expect admin balance to be (1 - 0.5) ether worth of tokens after minter1 burns 0.5 ether worth of tokens on behalf of admin:
+     ownerBalance = await VTZYContract.balanceOf(admin.address);
+     expect(ownerBalance).to.be.eq(_1E18.div(2))
+  
+      // Attempt to burn more than existing fund (additional 0.1 ether worth of token)
+      let invalidBurnAmount = ethers.utils.parseUnits("0.1")
+  
+      await expect(VTZYContract.connect(minter1).transferFrom(admin.address,minter2.address, invalidBurnAmount)).to.be.revertedWith("VTZY: Invalid allowance amount specified.");
+    
+    });
+   })
 });
+
