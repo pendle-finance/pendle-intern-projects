f0137f897ff2a60475fc8a59df3d584de54714eb
victortanzy123 <83770436+victortanzy123@users.noreply.github.com>
2022-05-31 19:30:05 +0800

    organised test folders

--- test/VTZY_ERC20.ts
+++ test/VTZY_ERC20.ts
@@ -5,45 +5,6 @@ import { deploy, evm_revert, evm_snapshot } from "./helpers/hardhat-helpers";
 import { TestContract, ERC20 } from "../typechain";
 import { ZERO_ADDRESS, _1E18 } from "./helpers/Constants";
 
-// describe("TestContract", () => {
-//   const [admin] = waffle.provider.getWallets();
-//   let globalSnapshotId;
-//   let snapshotId;
-//   let testContract: TestContract;
-
-//   before(async () => {
-//     globalSnapshotId = await evm_snapshot();
-
-//     testContract = await deploy<TestContract>("TestContract", []);
-//     await testContract.setTotal(100);
-
-//     snapshotId = await evm_snapshot();
-//   });
-
-//   async function revertSnapshot() {
-//     await evm_revert(snapshotId);
-//     snapshotId = await evm_snapshot();
-//   }
-
-//   beforeEach(async () => {
-//     await revertSnapshot();
-//   });
-
-//   it("increases total successfully", async () => {
-//     await testContract.increaseTotal(100);
-
-//     let curTotal = await testContract.getTotal();
-//     expect(curTotal).to.be.eq(200);
-//   });
-
-//   it("decreases total successfully", async () => {
-//     await testContract.decreaseTotal(50);
-
-//     let curTotal = await testContract.getTotal();
-//     expect(curTotal).to.be.eq(50);
-//   });
-// });
-
 
 describe("Testing VTZY ERC20 Contract", () => {
   const [admin, minter1, minter2] = waffle.provider.getWallets();
