452e660ff13f55be0f9a0098c85f61205f2f4f9b
LongNguyen <longnguyenhsgs@gmail.com>
2022-06-03 06:46:50 +0800

    add Metadata

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,24 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "./IERC20.sol";
+import "./IERC20Metadata.sol";
 
-contract ERC20 is IERC20 {
+contract ERC20 is  IERC20Metadata{
 
   uint private totalBalance;
   //address public owner;
   mapping(address => uint) private balances;
   mapping(address => mapping(address => uint)) private allowBalances;
-
+  string private tokenName;
+  string private tokenSymbol;
+  uint8 private tokenDecimal;
 
   // function owners() external view returns (address) {
   //   return owner;
   // }
 
-  constructor(uint value){
+  constructor(uint value, string memory _name, string memory _symbol, uint8 _decimal){
     //owner = msg.sender;
     totalBalance = value;
     balances[msg.sender] = value;
+    tokenName = _name;
+    tokenSymbol = _symbol;
+    tokenDecimal = _decimal;
   }
   function totalSupply() external view returns (uint256) {
     return totalBalance;
@@ -71,4 +76,15 @@ contract ERC20 is IERC20 {
     return true;
   }
 
+  function name() external view returns (string memory) {
+    return tokenName;
+  }
+
+  function symbol() external view returns (string memory) {
+    return tokenSymbol;
+  }
+
+  function decimals() external view returns (uint8) {
+    return tokenDecimal;
+  }
 }
