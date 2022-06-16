c15256ed94e28fa4e2ca406f34e1a1d7fa89da7b
Doraiskindacute <phuc@secrettune.io>
2022-06-17 01:28:09 +0700

    added helpers

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "./IDistributor.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./helpers/IERC20.sol";
 import "../node_modules/hardhat/console.sol";
 
 contract Distributor is IDistributor {
