aa94af238225d08c530f2128b0f1a821598788e2
LongNguyen <longnguyenhsgs@gmail.com>
2022-06-17 10:05:41 +0800

    fix

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -16,9 +16,13 @@ contract Airdrop is Ownable {
 
     mapping(address => uint) private ethBalances;
 
+    // owner address to token address to bool
+    mapping(address => mapping(address => bool)) private checkListOwnToken;
+    mapping(address => address[]) private listTokenOfOwner;
+
     // modifier
     modifier onlyNonzeroAddress(address check) {
-        require(check != address(0), "Invalid zero address");
+        require(check != address(0), "Zero address");
         _;
     }
 
@@ -29,16 +33,33 @@ contract Airdrop is Ownable {
     }
 
     function alowERC20(address receipt,address token,uint amount) public 
-                                                                  onlyOwner onlyNonzeroAddress(receipt)onlyNonzeroAddress(token) 
+                                                                  onlyOwner onlyNonzeroAddress(receipt) onlyNonzeroAddress(token) 
     {
         IERC20Metadata erc20token = IERC20Metadata(token);
         balances[receipt][token]+=amount;
+        // Add token if not already in list
+        if(!checkListOwnToken[receipt][token]){
+            checkListOwnToken[receipt][token] = true;
+        }
+        listTokenOfOwner[receipt].push(token);
 
         emit Setting(receipt,erc20token.name(),amount);
     }
 
-    function claim(address[] memory tokens,uint[] memory amounts, bool eth,uint ethAmount) public {
-        for(uint i=0;i<tokens.length;i++) {
+    function claimAll() external {
+        if(ethBalances[msg.sender]>0){
+            require(_ethtransfer(payable(msg.sender), ethBalances[msg.sender]),"Transfer eth fail");
+        }
+        uint tokenLength = listTokenOfOwner[msg.sender].length;
+        for(uint i = 0 ;i <tokenLength;i++ ) {
+            require(_erc20transfer(msg.sender, listTokenOfOwner[msg.sender][i], balances[msg.sender][listTokenOfOwner[msg.sender][i]]),"Transfer token fail");
+        }
+        delete listTokenOfOwner[msg.sender];
+    }
+    function claim(address[] memory tokens,uint[] memory amounts, bool eth,uint ethAmount) external {
+        require(tokens.length == amounts.length,"Different length");
+        uint tokenLength = tokens.length;
+        for(uint i=0;i<tokenLength;i++) {
             require(_erc20transfer(msg.sender, tokens[i], amounts[i]),"Transfer token fail");
         }
         if(eth){
@@ -63,28 +84,22 @@ contract Airdrop is Ownable {
     {
         require(balances[msg.sender][token]>=amount,"Insufficient balance");
         IERC20Metadata erc20token = IERC20Metadata(token);
-        // do nothing with 0 amount
-        if(amount > 0) {
-            balances[msg.sender][token]-=amount;
-            erc20token.transfer(receipt, amount);        
-        }
+        //require(amount>0,"Zero amount token transfer");
+        balances[msg.sender][token]-=amount;
+        erc20token.transfer(receipt, amount);        
         emit Transfer(receipt, erc20token.name(), amount);
-
         return true;
     }
 
-    function _ethtransfer(address payable receipt, uint amount) internal 
-                                                                onlyNonzeroAddress(receipt) returns (bool)
+    function _ethtransfer(address payable receipt, uint amount) internal onlyNonzeroAddress(receipt) returns (bool)
     {
         require(ethBalances[receipt]>=amount,"Insufficient balance");
-        // Do nothing with zero amount
-        if(amount > 0) {
-            ethBalances[receipt]-=amount;
+        //require(amount>0,"Zero amount eth transfer");
+        ethBalances[receipt]-=amount;
 
-            receipt.transfer(amount);
+        receipt.transfer(amount);
 
-            emit TransferETH(msg.sender,amount);
-        }
+        emit TransferETH(msg.sender,amount);
         return true;
     }
 
