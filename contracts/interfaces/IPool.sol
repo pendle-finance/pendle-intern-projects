dac5266006dc199646faaf2991889fa1f6f52324
HOW RUI YANG <25howry@gmail.com>
2022-06-20 14:50:52 +0800

    Minor edits to interfaces, added IERC20Metadata

--- contracts/interfaces/IPool.sol
+++ contracts/interfaces/IPool.sol
@@ -1,28 +1,8 @@
 pragma solidity ^0.8.0;
 
-interface IPool {
-  function name() external pure returns (string memory);
+import "./IERC20Metadata.sol";
 
-  function symbol() external pure returns (string memory);
-
-  function decimals() external pure returns (uint8);
-
-  function totalSupply() external view returns (uint256);
-
-  function balanceOf(address owner) external view returns (uint256);
-
-  function allowance(address owner, address spender) external view returns (uint256);
-
-  function approve(address spender, uint256 value) external returns (bool);
-
-  function transfer(address to, uint256 value) external returns (bool);
-
-  function transferFrom(
-    address from,
-    address to,
-    uint256 value
-  ) external returns (bool);
-
+interface IPool is IERC20Metadata {
   function DOMAIN_SEPARATOR() external view returns (bytes32);
 
   function PERMIT_TYPEHASH() external pure returns (bytes32);
