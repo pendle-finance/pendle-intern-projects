d770e655f150e79594c30684148dcc0949c50eda
lamsecrettune <lam@secrettune.io>
2022-06-20 10:21:20 +0700

    interfaces

---
+++ contracts/interfaces/IFactory.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+interface IFactory {
+  event PoolCreated(address indexed token0, address indexed token1, address pair, uint256);
+
+  function getPool(address tokenA, address tokenB) external view returns (address pool);
+
+  function allPool(uint256) external view returns (address pool);
+
+  function allPoolLength() external view returns (uint256);
+
+  function createPool(address tokenA, address tokenB) external returns (address pool);
+}
