e4687f16354127f90d0cf7c1c39b35b034e75932
lamsecrettune <lam@secrettune.io>
2022-06-22 11:47:54 +0700

    config

--- contracts/libraries/UQ112x112.sol
+++ contracts/libraries/UQ112x112.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.5.16;
+pragma solidity ^0.8.0;
 
 // a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
 
@@ -6,15 +6,15 @@ pragma solidity =0.5.16;
 // resolution: 1 / 2**112
 
 library UQ112x112 {
-    uint224 constant Q112 = 2**112;
+  uint224 constant Q112 = 2**112;
 
-    // encode a uint112 as a UQ112x112
-    function encode(uint112 y) internal pure returns (uint224 z) {
-        z = uint224(y) * Q112; // never overflows
-    }
+  // encode a uint112 as a UQ112x112
+  function encode(uint112 y) internal pure returns (uint224 z) {
+    z = uint224(y) * Q112; // never overflows
+  }
 
-    // divide a UQ112x112 by a uint112, returning a UQ112x112
-    function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
-        z = x / uint224(y);
-    }
+  // divide a UQ112x112 by a uint112, returning a UQ112x112
+  function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
+    z = x / uint224(y);
+  }
 }
