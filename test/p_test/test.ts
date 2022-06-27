79c441935f84d719b1edda6ac4d9f6459d45155a
Ddorsmt <phuc@secrettune.io>
2022-06-28 01:08:40 +0700

    test ready

--- test/p_test/test.ts
+++ test/p_test/test.ts
@@ -2,19 +2,23 @@ import { expect } from "chai";
 import { utils } from "ethers";
 import { ethers, waffle } from "hardhat";
 import { deploy, evm_revert, evm_snapshot } from "../helpers/hardhat-helpers";
-import { TestContract } from ".../typechain";
-
-describe("TestContract", () => {
+import { ERC20, Router, LPFactory } from "../../typechain";
+describe("Router", () => {
   const [admin] = waffle.provider.getWallets();
   let globalSnapshotId;
   let snapshotId;
-  let testContract: TestContract;
+  let coin1, coin2, coin3: ERC20;
+  let factory: LPFactory;
+  let router: Router;
 
   before(async () => {
     globalSnapshotId = await evm_snapshot();
 
-    testContract = await deploy<TestContract>("TestContract", []);
-    await testContract.setTotal(100);
+    coin1 = await deploy<ERC20>("ERC20", [100, "peepeepoopoo", "PPPOPO", 1]);
+    coin2 = await deploy<ERC20>("ERC20", [100, "poopoopeepee", "POPOPP", 1]);
+    coin2 = await deploy<ERC20>("ERC20", [100, "peepoopeepoo", "PPOPPO", 1]);
+    factory = await deploy<LPFactory>("LPFasctory", []);
+    router = await deploy<Router>("router", [factory.address]);
 
     snapshotId = await evm_snapshot();
   });
@@ -27,18 +31,5 @@ describe("TestContract", () => {
   beforeEach(async () => {
     await revertSnapshot();
   });
-
-  it("increases total successfully", async () => {
-    await testContract.increaseTotal(100);
-
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(200);
-  });
-
-  it("decreases total successfully", async () => {
-    await testContract.decreaseTotal(50);
-
-    let curTotal = await testContract.getTotal();
-    expect(curTotal).to.be.eq(50);
-  });
+  
 });
