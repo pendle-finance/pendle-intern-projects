45e2bce67c457bb30235ce0eb40e512a832d0d9f
Ddorsmt <phuc@secrettune.io>
2022-06-28 00:32:29 +0700

    added stuff

--- contracts/p_implement/Helper.sol
+++ contracts/p_implement/Helper.sol
@@ -1,33 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.11;
 
 import "../interface/ILPPair.sol";
+import "../interface/ILPFactory.sol";
 
 contract Helper {
 
-  function min(address tokenA, address tokenB) internal view returns (address) {
+  function min(address tokenA, address tokenB) internal pure returns (address) {
     return tokenA < tokenB ? tokenA : tokenB;
   }
 
   function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint256, uint256) {
     address token0 = min(tokenA, tokenB);
-    (uint256 reserve0, uint256 reserve1) = ILPPair(pairFor(factory, tokenA, tokenB)).getReserves();
+    (uint256 reserve0, uint256 reserve1) = ILPPair(ILPFactory(factory).getPair(tokenA, tokenB)).getReserves();
     return tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
   }
 
-  function calculateTrade(uint256 amountA, uint256 reserveA, uint256 reserveB) internal view returns (uint256) {
+  function calculateTrade(uint256 amountA, uint256 reserveA, uint256 reserveB) internal pure returns (uint256) {
     require(amountA > 0, "stop trolling :(");
     require(reserveA > 0 && reserveB > 0, "liquid evaporated :(");
     return amountA * reserveB / reserveA;
   }
 
-  function exactIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut) internal view returns (uint256) {
+  function exactIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut) internal pure returns (uint256) {
     require(amountOut > 0, "stop trolling :(");
     require(reserveIn > 0 && reserveOut > 0, "liquid evaporated :(");
     return (amountOut * reserveIn / (reserveOut - amountOut)) + 1;
   }
 
-  function exactOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) internal view returns (uint256) {
+  function exactOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) internal pure returns (uint256) {
     require(amountIn > 0, "stop trolling :(");
     require(reserveIn > 0 && reserveOut > 0, "liquid evaporated :(");
     return amountIn * reserveOut / (reserveIn + amountIn);
@@ -40,7 +41,7 @@ contract Helper {
 
     for (uint256 i = pathLength - 1; i > 0; i--) {
       (uint256 reserveIn, uint256 reserveOut) = getReserves(factory, path[i - 1], path[i]);
-      amounts[i - 1] = getAmountIn(amounts[i], reserveIn, reserveOut);
+      amounts[i - 1] = exactIn(amounts[i], reserveIn, reserveOut);
     }
   }
 
@@ -51,7 +52,7 @@ contract Helper {
 
     for (uint256 i = 0; i < pathLength - 1; i++) {
       (uint256 reserveIn, uint256 reserveOut) = getReserves(factory, path[i], path[i + 1]);
-      amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut);
+      amounts[i + 1] = exactOut(amounts[i], reserveIn, reserveOut);
     }
   }
 }
